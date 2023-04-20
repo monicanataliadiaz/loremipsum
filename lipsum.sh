e8e132192c1264549d0528bdc816e98f8b09f1cb
monicanataliadiaz <nattaccha@gmail.com>
2023-04-20 10:17:37 -0300

    a

---
+++ lipsum.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#shebang se  lo de arriba q le dice a linux q es ejecutable y debe ejecutarlo con el programa bash
+#man bash en terminal da info de como funciona, con bash lipsum.sh lo ejecuto y genera los 5 txt
+for i in {1..5}
+do
+    #obtiene una cantidad aleatoria de número de lineas entre 1 y 20
+    lineas=$(shuf -i 1-20 -n 1)
+    #llama con curl a lipsum.com y le pasa como parámetro la cantidad de líneas
+    lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g')   
+    #crea un archivo con el contenido de la variable lorem
+    echo "$lorem" >> loremipsum-$i.txt
+done
